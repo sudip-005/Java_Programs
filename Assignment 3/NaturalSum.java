398cd68e183ea12f6abd75dc1af6c2c1156e12c7
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-16 08:20:34 +0530

    Add files via upload

---
+++ Assignment 3/NaturalSum.java
@@ -0,0 +1,8 @@
+class NaturalSum {
+    public static void main(String[] args) {
+        int n=5, sum=0;
+        for(int i=1;i<=n;i++)
+            sum+=i;
+        System.out.println("Sum = "+sum);
+    }
+}
---
+++ Assignment 3/StringMethods.java
@@ -0,0 +1,10 @@
+class StringMethods {
+    public static void main(String[] args) {
+        String s="Hello";
+        System.out.println(s.charAt(1));
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.substring(1,4));
+    }
+}
