4ec179ea86ab0a91ff14beb3f0e381e4293a2e97
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-16 08:17:40 +0530

    Add files via upload

---
+++ Assignment 3/ScannerDemo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        System.out.println("You entered: " + n);
+    }
+}
