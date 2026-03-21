3fe262dc7cd7d7b360fad998a0b67a6f965ce6e2
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-21 23:43:39 +0530

    Create Q6.java

---
+++ Assignment 5/Q6.java
@@ -0,0 +1,16 @@
+class A {
+    void show() {
+        System.out.println("Class A");
+    }
+}
+
+class B {
+    void show() {
+        System.out.println("Class B");
+    }
+}
+
+/*
+class C extends A, B { }
+This will give compile time error because Java does not allow multiple inheritance.
+*/
