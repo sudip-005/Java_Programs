ae7c6e3f1ee0c646f4fedc251210f5a7b948137e
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-22 22:17:29 +0530

    Create Q7.java

---
+++ Assignment 5/Q7.java
@@ -0,0 +1,16 @@
+class Parent {
+    void show() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child extends Parent {
+    void show() {
+        System.out.println("Child Method");
+    }
+
+    public static void main(String[] args) {
+        Child obj = new Child();
+        obj.show();
+    }
+}
