c5736771bc790ba4c7a72adaec8e9a6181a5c69f
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-25 23:06:16 +0530

    Create Q11.java

---
+++ Assignment 6/Q11.java
@@ -0,0 +1,33 @@
+class Employee {
+
+    int id = 101;
+    String name = "Rahul";
+}
+
+class Department extends Employee {
+
+    String deptName = "IT";
+    int deptCode = 10;
+}
+
+class Salary extends Department {
+
+    double basic = 20000;
+    double allowance = 5000;
+
+    void display() {
+
+        double total = basic + allowance;
+
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + deptName);
+        System.out.println("Total Salary: " + total);
+    }
+
+    public static void main(String[] args) {
+
+        Salary s = new Salary();
+        s.display();
+    }
+}
