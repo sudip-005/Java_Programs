25bff102695cd780b3e5817d1d0cdd5aacad727a
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-04-12 23:04:35 +0530

    Add Q4.java with exception handling example

---
+++ Assignment 8/Q4.java
@@ -0,0 +1,11 @@
+class Demo {
+    public static void main(String[] args) {
+        try {
+            int a = 10/0;
+        } catch(ArithmeticException e) {
+            System.out.println("Arithmetic Error");
+        } catch(Exception e) {
+            System.out.println("General Error");
+        }
+    }
+}
