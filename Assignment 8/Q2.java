2b791e61f1ed0f78e05769dc6a688272629e32a2
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-04-12 23:03:58 +0530

    Add exception handling in Q2.java

---
+++ Assignment 8/Q2.java
@@ -0,0 +1,9 @@
+class Demo {
+    public static void main(String[] args) {
+        try {
+            int a = 10/0;
+        } catch(Exception e) {
+            System.out.println("Exception Handled");
+        }
+    }
+}
